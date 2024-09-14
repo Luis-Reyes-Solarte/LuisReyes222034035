a306c786936308d7edb06cdab487968799ea6b9b
Luis Reyes Solarte <reyesluiscarlos920@gmail.com>
2024-09-14 11:00:19 -0500

    feat: create components/modules

--- clase-3/src/app/app.component.ts
+++ clase-3/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MytestcomponentComponent } from "./mytestcomponent/mytestcomponent.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MytestcomponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- clase-3/src/app/app.routes.ts
+++ clase-3/src/app/app.routes.ts
@@ -1,3 +1,20 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { MytestcomponentComponent } from './mytestcomponent/mytestcomponent.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '', //para que se corra en el localhost por defecto
+    component: MytestcomponentComponent
+    },
+    {
+        path:'servicios',
+        component:ServiciosComponent
+    },
+    {
+        path:'sidebar',
+        component:SidebarComponent
+    }
+];
