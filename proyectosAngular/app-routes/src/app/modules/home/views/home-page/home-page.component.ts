fccf9718295e1d59e6d248e2a564a35fe30e28e0
Luis Reyes Solarte <reyesluiscarlos920@gmail.com>
2024-09-21 10:49:32 -0500

    feat: uso primeng

--- proyectosAngular/app-routes/src/app/modules/home/views/home-page/home-page.component.ts
+++ proyectosAngular/app-routes/src/app/modules/home/views/home-page/home-page.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { SidebarPageComponent } from "../../../sidebar/views/sidebar-page/sidebar-page.component";
+import { AuthPageComponent } from "../../../auth/views/auth-page/auth-page.component";
+import { SidetopPageComponent } from "../../../sidetop/views/sidetop-page/sidetop-page.component";
+import { PrimePageComponent } from "../../../UI/views/prime-page/prime-page.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [SidebarPageComponent, 
+    AuthPageComponent, SidetopPageComponent, 
+    PrimePageComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
