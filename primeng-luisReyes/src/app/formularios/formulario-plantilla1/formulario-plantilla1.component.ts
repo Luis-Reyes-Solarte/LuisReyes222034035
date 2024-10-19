636368f4b3488348d66160d6c0da133d36afea98
Luis-Reyes-Solarte <reyesluiscarlos920@gmail.com>
2024-10-19 08:25:01 -0500

    feat: create components 'formularios'

---
+++ primeng-luisReyes/src/app/formularios/formulario-plantilla1/formulario-plantilla1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-plantilla1',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulario-plantilla1.component.html',
+  styleUrl: './formulario-plantilla1.component.css'
+})
+export class FormularioPlantilla1Component {
+
+}
