ad5dd2a56731f2e0bd81c56ac14a3437bdd3d43b
Luis-Reyes-Solarte <reyesluiscarlos920@gmail.com>
2024-10-19 09:07:55 -0500

    fix: some bugs in styles

--- primeng-luisReyes/src/app/app.component.ts
+++ primeng-luisReyes/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { FormularioPlantilla1Component } from './formularios/formulario-plantilla1/formulario-plantilla1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ButtonModule],
+  imports: [RouterOutlet,ButtonModule,
+    FormularioPlantilla1Component
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
