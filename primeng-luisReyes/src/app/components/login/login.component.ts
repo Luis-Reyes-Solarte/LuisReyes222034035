6162cd72f6738e3fa9cecdbefa618860cc977abb
Luis-Reyes-Solarte <reyesluiscarlos920@gmail.com>
2024-10-19 11:51:48 -0500

    feat: some changes

--- primeng-luisReyes/src/app/components/login/login.component.ts
+++ primeng-luisReyes/src/app/components/login/login.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroupDirective, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CardModule,InputTextModule],
+  imports: [CardModule,InputTextModule,FormGroupDirective, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  
+  loginForm = this.fb.group({
+    usuario: ['',[Validators.required], Validators.name],
+    password: ['',Validators.required]
+  });
+
+constructor(private fb: FormBuilder){}
 
 }
