f31d2d27940a6a2e1eeb2395e33294d5efc0a2a1
Luis-Reyes-Solarte <reyesluiscarlos920@gmail.com>
2024-10-19 10:24:48 -0500

    feat: install primeflex

--- primeng-luisReyes/src/app/app.component.ts
+++ primeng-luisReyes/src/app/app.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { FormularioPlantilla1Component } from './formularios/formulario-plantilla1/formulario-plantilla1.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,ButtonModule,
-    FormularioPlantilla1Component
+    FormularioPlantilla1Component, HomeComponent,
+    LoginComponent, RegisterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
