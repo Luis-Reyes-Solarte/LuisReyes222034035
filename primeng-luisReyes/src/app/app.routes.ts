847c22d33b9f2f83ff32223b1f2f7c52873e91f7
Luis Reyes Solarte <reyesluiscarlos920@gmail.com>
2024-10-26 08:36:15 -0500

    feat: login-test finished

--- primeng-luisReyes/src/app/app.routes.ts
+++ primeng-luisReyes/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { LoginTestComponent } from './components/login-test/login-test.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
-    {path:'',redirectTo:'home',pathMatch:'full'}
+    {path:'',redirectTo:'home',pathMatch:'full'},
+    {path: 'logintest', component: LoginTestComponent}
 ];
