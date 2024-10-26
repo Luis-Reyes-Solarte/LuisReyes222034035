10a0a3fd31a53b27c9b44af21b8bbb866c6221d5
Luis Reyes Solarte <reyesluiscarlos920@gmail.com>
2024-10-26 08:36:38 -0500

    feat: login-test finished 2

--- primeng-luisReyes/src/app/components/login-test/login-test.component.ts
+++ primeng-luisReyes/src/app/components/login-test/login-test.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-login-test',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,InputTextModule,ButtonModule,PasswordModule,], //modulos requeridos para el trabajo con formularios
   templateUrl: './login-test.component.html',
   styleUrl: './login-test.component.css'
 })
 export class LoginTestComponent {
 
+  userForm: FormGroup;
+
+  constructor(private fb:FormBuilder){
+    this.userForm = this.fb.group({
+      name: ['',Validators.required],
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.minLength(6)]]
+    })
+  }
+
+  onSubmit(){
+    if(this.userForm.valid){
+      console.log(this.userForm.value);
+    }else{
+      console.log('formulario invalido')
+    }
+  }
 }
--- primeng-luisReyes/src/app/components/login/login.component.ts
+++ primeng-luisReyes/src/app/components/login/login.component.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroupDirective, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroupDirective, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CardModule,InputTextModule,FormGroupDirective, ReactiveFormsModule],
+  imports: [CardModule,InputTextModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   
-  loginForm = this.fb.group({
+  loginForm: FormGroup;
+
+constructor(private fb: FormBuilder){
+  this.loginForm = this.fb.group({
     usuario: ['',[Validators.required], Validators.name],
     password: ['',Validators.required]
-  });
-
-constructor(private fb: FormBuilder){}
+  })
+  
+}
 
 }
