95bd13ba2c4a794304e9c8faccc14d230e229229
Luis Reyes Solarte <reyesluiscarlos920@gmail.com>
2024-10-26 07:51:03 -0500

    feat: new component "login-test"

---
+++ primeng-luisReyes/src/app/components/login-test/login-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-test.component.html',
+  styleUrl: './login-test.component.css'
+})
+export class LoginTestComponent {
+
+}
