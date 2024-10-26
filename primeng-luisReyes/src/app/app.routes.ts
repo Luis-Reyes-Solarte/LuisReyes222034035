a7c97f5b94d1c3a9dc4455c8b6721e07a900eb04
Luis Reyes Solarte <reyesluiscarlos920@gmail.com>
2024-10-26 10:22:14 -0500

    feat: some things

--- primeng-luisReyes/src/app/app.routes.ts
+++ primeng-luisReyes/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginTestComponent } from './components/login-test/login-test.component';
+import { OtherFormComponent } from './components/other-form/other-form.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     {path:'',redirectTo:'home',pathMatch:'full'},
-    {path: 'logintest', component: LoginTestComponent}
+    {path: 'logintest', component: LoginTestComponent},
+    {path: 'otherform', component: OtherFormComponent}
 ];
---
+++ primeng-luisReyes/src/app/components/other-form-cb/other-form-cb.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other-form-cb',
+  standalone: true,
+  imports: [],
+  templateUrl: './other-form-cb.component.html',
+  styleUrl: './other-form-cb.component.css'
+})
+export class OtherFormCbComponent {
+
+}
