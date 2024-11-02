982d25f98a30f04bb5a1abb791ac34bccd008773
Luis-Reyes-Solarte <reyesluiscarlos920@gmail.com>
2024-11-01 22:40:13 -0500

    feat: some changes

--- formulario/src/app/app.component.ts
+++ formulario/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Parte1Component } from './components/parte1/parte1.component';
+import { Parte2Component } from './components/parte2/parte2.component';
+import { Parte3Component } from './components/parte3/parte3.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Parte1Component,Parte2Component,Parte3Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- formulario/src/app/components/parte1/parte1.component.ts
+++ formulario/src/app/components/parte1/parte1.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-parte1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './parte1.component.html',
   styleUrl: './parte1.component.css'
 })
 export class Parte1Component {
 
+  userForm: FormGroup;
+
+  constructor(private fb:FormBuilder){
+    this.userForm = this.fb.group({
+      nombre: ['',Validators.required],
+      apellido: ['',Validators.required],
+      email: ['',Validators.required,Validators.email],
+      telefono: ['',Validators.required],
+    })
+  }
 }
