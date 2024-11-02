74c86f61148ebcc7808949189f70b334aa8beab4
Luis-Reyes-Solarte <reyesluiscarlos920@gmail.com>
2024-11-01 23:52:11 -0500

    feat: parte3 some changes

--- formulario/src/app/components/parte3/parte3.component.ts
+++ formulario/src/app/components/parte3/parte3.component.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+
 
 @Component({
   selector: 'app-parte3',
   standalone: true,
-  imports: [CommonModule,CalendarModule],
+  imports: [CommonModule,CalendarModule,DropdownModule],
   templateUrl: './parte3.component.html',
   styleUrl: './parte3.component.css'
 })
@@ -16,9 +18,8 @@ export class Parte3Component {
   constructor(private fb:FormBuilder){
     this.userForm3 = this.fb.group({
       identificacion: ['',Validators.required],
-      apellido: ['',Validators.required],
-      email: ['',Validators.required,Validators.email],
-      telefono: ['',Validators.required],
+      calendario1: ['',Validators.required],
+      calendario2: ['',Validators.required],
     })
   }
 }
