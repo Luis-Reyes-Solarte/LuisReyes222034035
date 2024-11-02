dc987cb9dfa9b8307fbdba37eef5f00298ed6e97
Luis-Reyes-Solarte <reyesluiscarlos920@gmail.com>
2024-11-01 23:26:31 -0500

    feat: end of parte1 and parte2

--- formulario/src/app/components/parte2/parte2.component.ts
+++ formulario/src/app/components/parte2/parte2.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-parte2',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './parte2.component.html',
   styleUrl: './parte2.component.css'
 })
 export class Parte2Component {
 
+  userForm2: FormGroup;
+
+  constructor(private fb:FormBuilder){
+    this.userForm2 = this.fb.group({
+      nombre_gerente: ['',Validators.required],
+      email_gerente: ['',Validators.required,Validators.email],
+    })
+  }
 }
