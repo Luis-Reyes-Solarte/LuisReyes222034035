a120a9a9791ce155303d9de97e7751290e38dc75
Luis-Reyes-Solarte <reyesluiscarlos920@gmail.com>
2024-11-01 23:54:26 -0500

    feat: finsishing parte3

--- formulario/src/app/components/parte3/parte3.component.ts
+++ formulario/src/app/components/parte3/parte3.component.ts
@@ -2,13 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
-import { DropdownModule } from 'primeng/dropdown';
 
 
 @Component({
   selector: 'app-parte3',
   standalone: true,
-  imports: [CommonModule,CalendarModule,DropdownModule],
+  imports: [CommonModule,CalendarModule,],
   templateUrl: './parte3.component.html',
   styleUrl: './parte3.component.css'
 })
