61b30e92666091b275379f45bd3c6af14fd84e49
Luis-Reyes-Solarte <reyesluiscarlos920@gmail.com>
2024-11-03 00:08:25 -0500

    fix: some changes

--- formulario/src/app/app.component.ts
+++ formulario/src/app/app.component.ts
@@ -5,12 +5,15 @@ import { Parte2Component } from './components/parte2/parte2.component';
 import { Parte3Component } from './components/parte3/parte3.component';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,Parte1Component,Parte2Component,
-    Parte3Component,CardModule,InputTextModule],
+    Parte3Component,CardModule,InputTextModule,
+    ReactiveFormsModule,ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
